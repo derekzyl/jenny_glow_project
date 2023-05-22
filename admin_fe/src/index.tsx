ef77d444a3741fd634e31692d11264811f2e2b74
derek og <derekzyl@gmail.com>
2023-05-22 10:01:23 +0100

    first push

---
+++ admin_fe/src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'nprogress/nprogress.css';
+import App from 'src/App';
+import { SidebarProvider } from 'src/contexts/SidebarContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <SidebarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SidebarProvider>
+  </HelmetProvider>
+);
+// If you want your app to work offline and load faster, you can change
