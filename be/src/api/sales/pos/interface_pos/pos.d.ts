fc3a30551b188a8a28fda6c2363b35602d4590ed
derek og <derekzyl@gmail.com>
2023-05-29 11:24:57 +0100

    online sales

--- be/src/api/sales/pos/interface_pos/pos.d.ts
+++ be/src/api/sales/pos/interface_pos/pos.d.ts
@@ -8,28 +8,7 @@ import {
   OrderStatusE,
 } from "../../interface_sales/sales";
 
-// interface PosI extends Document {
-//   order_id: string;
-//   status: StatusE;
-//   type: SalesTypeE;
-//   original_amount: number;
-//   amount_sold: number;
-//   discount: number;
-//   sold_by: Types.objectId;
-//   product: {
-//     product: Types.ObjectId;
-//     quantity: number;
-//     total: number;
-//   };
-//   dispatch: {
-//     is_dispatched: boolean;
-//     dispatched_by: Types.objectId;
-//     dispatched_at: Date;
-//   };
 
-//   is_delivered: boolean;
-//   message: MessageT[];
-// }
 
 export interface PosI extends Document {
   order_id: string;
